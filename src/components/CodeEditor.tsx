f17563c3641c936e0aa9298faa701c02cbc388a0
AnnuTamilarasu <ananya.tamilarasu@gmail.com>
2025-11-03 20:37:11 -0500

    //everything works p3.2...

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import CodeMirror from "@uiw/react-codemirror";
 import { javascript } from "@codemirror/lang-javascript";
 import { python } from "@codemirror/lang-python";
